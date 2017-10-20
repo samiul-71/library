7e2c57e65c3e9bb0db31bbcd8be249ceb1a4ecd8
shahlalhossain <shahlalhossain@gmail.com>
2017-10-20 23:48:36 +0600

    Re-structure the Admin Dashboard Page

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -16,7 +16,7 @@
             </div><!-- /.box tools -->
         </div><!-- /.box-header -->
         <div class="box-body">
-            {!! trans('strings.backend.welcome') !!}
+            {{--{!! trans('strings.backend.welcome') !!}--}}
         </div><!-- /.box-body -->
     </div><!--box box-success-->
 
